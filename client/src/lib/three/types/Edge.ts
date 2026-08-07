09a8af2db753ea4231f1b314bd4b27b3e3e3b228
fariswirakusuma <fariswkt@gmail.com>
2026-08-07 14:46:42 +0700

    refactor: merge all type into index.ts

--- client/src/lib/three/types/Edge.ts
+++
@@ -1,7 +0,0 @@
-import type { Node } from '../types/Node';
-
-export interface Edge {
-  targetNode: Node;
-  weight: number;
-  isBlocked?: boolean;
-}
--- client/src/lib/three/types/index.ts
+++ client/src/lib/three/types/index.ts
@@ -1,13 +1,47 @@
-import type { Edge } from './Edge.ts';
-import type { Obstacle } from './Obstacle.ts';
-import type { Node } from './Node.ts';
-
-// Reexport your entry components here
-export * from './Node';
-export * from './Edge';
-export * from './Obstacle'; 
+import * as THREE from 'three';
 
 export type PathfindingAlgorithm = 'a-star' | 'dijkstra' | 'bellman-ford';
+export interface Point3D {
+  x: number;
+  y: number;
+  z: number;
+}
+
+export interface Obstacle {
+  id: string;
+  nodeId: string;
+  position: Point3D | THREE.Vector3;
+  width: number;
+  height: number;
+  depth: number;
+}
+
+export interface Edge {
+  targetNode: Node;
+  weight: number;
+  isBlocked?: boolean;
+}
+
+export interface Node {
+  id: string;
+  position: THREE.Vector3|Point3D;
+  neighbors?: Edge[];
+  parent?: Node;
+  floor?: number;
+  name?: string;
+  
+}
+
+export interface ArrowNode {
+    FromNodeId: string;
+    ToNodeId: string;
+    Floor: number;
+    Status: string;
+    Position: Point3D;
+    Direction: Point3D;
+}
+
+
 
 export interface PathfindingRequest {
   startNodeId: string;
